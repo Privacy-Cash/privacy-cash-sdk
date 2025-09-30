8477d7b6512777783bf64fa575aaf06385e48120
stanNthe5 <st@jcow.net>
2025-09-30 23:47:39 +0800

    fix localstorage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,14 +1,15 @@
 export async function getStorageInstance(): Promise<Storage> {
-    if (typeof window !== "undefined" && window.localStorage) {
-        // for web browser
+    if (typeof window !== "undefined" && typeof window.localStorage !== "undefined") {
+        // web browser
         return window.localStorage;
-    } else {
-        // for Node
+    } else if (typeof process !== "undefined" && process.versions?.node) {
+        // Node runtime
         if (!localStorage) {
             const { LocalStorage, path } = await import("./node-shim.js");
             localStorage = new LocalStorage(path.join(process.cwd(), "cache"));
-            return localStorage
         }
-        return localStorage
+        return localStorage;
+    } else {
+        throw new Error("Unknown environment: cannot determine storage");
     }
 }
