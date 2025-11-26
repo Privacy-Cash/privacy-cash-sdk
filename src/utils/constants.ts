07f1916266aaf949f1526c058e6d04d9578506c1
stanNthe5 <st@jcow.net>
2025-11-27 05:25:49 +0800

    change program id

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import BN from 'bn.js';
 
 export const FIELD_SIZE = new BN('21888242871839275222246405745257275088548364400416034343698204186575808495617')
 
-export const PROGRAM_ID = new PublicKey('ATZj4jZ4FFzkvAcvk27DW9GRkgSbFnHo49fKKPQXU7VS');
+export const PROGRAM_ID = new PublicKey('nhfZpgCLZXtVWgSNcXjsaTjqExbbyX3hDvEYUXYEpqf');
 
 export const DEPLOYER_ID = new PublicKey('97rSMQUukMDjA7PYErccyx7ZxbHvSDaeXp2ig5BwSrTf')
 
@@ -18,7 +18,7 @@ export const TRANSACT_SPL_IX_DISCRIMINATOR = Buffer.from([154, 66, 244, 204, 78,
 
 export const MERKLE_TREE_DEPTH = 26;
 
-export const ALT_ADDRESS = new PublicKey('6TufVuUYc2SqVLpR5YDLzWAogicpoS4mWShgPP7HXunm');
+export const ALT_ADDRESS = new PublicKey('4qsKAZfHGSucmZ8iZHRdifKpfX63T3CPrk3Vj4mNxcfo');
 
 export const RELAYER_API_URL = process.env.NEXT_PUBLIC_RELAYER_API_URL ?? 'https://staging.privacycash.org';
 
